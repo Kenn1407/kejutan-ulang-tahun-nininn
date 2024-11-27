f9a0bd2e04bbfacbc3f09212a7ef997888b7aafb
Kenn1407 <kennedykadafie014@gmail.com>
2024-11-28 00:58:11 +0700

    baru

---
+++ c.js
@@ -0,0 +1,6 @@
+<script>
+function toggleMessage() {
+    var message = document.getElementById("hiddenMessage");
+    message.classList.toggle("show"); // Tambahkan atau hapus kelas show
+}
+</script>
